c8989eb60b8be1c66f86687380fabda0d5270d3e
zsevic <zsevic95@gmail.com>
2019-11-19 13:03:12 +0100

    :wrench: Add config file

--- src/api/schedule/schedule.service.js
+++ src/api/schedule/schedule.service.js
@@ -1,25 +1,21 @@
+import { groups } from '../../config/constants';
 import { getSchedule } from '../../utils';
 
-const groups = [
-  'http://poincare.matf.bg.ac.rs/~kmiljan/raspored/sve/form_016.html', // 2i2a
-  'http://poincare.matf.bg.ac.rs/~kmiljan/raspored/sve/form_024.html', // 3i
-];
-
 export async function getScheduler() {
-  const schedule = [];
+  const classes = [];
   const boxCounters = [];
 
   for (let i = 0; i < groups.length; i += 1) {
     boxCounters.push(0);
-    schedule.push([]);
+    classes.push([]);
   }
 
-  const courses = groups.map(async (url, index) => getSchedule(url, index, boxCounters, schedule));
+  const courses = groups.map(async (url, index) => getSchedule(url, index, boxCounters, classes));
 
   try {
-    const newSchedule = await Promise.all(courses);
+    const schedule = await Promise.all(courses);
 
-    return newSchedule[0].flat();
+    return schedule[0].flat();
   } catch (e) {
     return { err: e.message };
   }
---
+++ src/config/constants.js
@@ -0,0 +1,4 @@
+export const groups = [
+  'http://poincare.matf.bg.ac.rs/~kmiljan/raspored/sve/form_016.html', // 2i2a
+  'http://poincare.matf.bg.ac.rs/~kmiljan/raspored/sve/form_024.html', // 3i
+];
